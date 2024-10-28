85f497a1dd76c528c77eed210f7c09dea6aac36d
jonathan <oregonjonathan@gmail.com>
2024-10-28 01:00:19 -0700

    test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,21 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a poop node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_hyperlink(self):
+        node = TextNode("This is a text node with link", TextType.ITALIC,"https://www.boot.dev/")
+        node2 = TextNode("This is a text node with link", TextType.ITALIC,"https://www.boot.dev/")
+        self.assertEqual(node, node2)
+    
+    def test_code(self):
+        node = TextNode("Hello, world!", TextType.CODE)
+        node2 = TextNode("Hello, world!", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
